c51d8c246a2938f768408836e348754ce2808b35
deekCS <31720001079@std.bau.edu.jo>
2024-02-22 21:10:20 +0300

    feat: Add useEffect hook to simple fetch recipes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { useEffect } from 'react'
+
+import { getRecipes } from './services/spoonacularApis'
 function App() {
+  useEffect(() => {
+    getRecipes()
+  }, [])
 
   return (
-   <div>
-       <h1>
-           Hello Desaisiv!
-       </h1>
-   </div>
+    <div>
+      <h1>Hello Desaisiv!</h1>
+    </div>
   )
 }
 
