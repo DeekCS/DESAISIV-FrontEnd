bdd863606b5d2b3dd9cee60ae8cb5d2042a2561b
deekCS <31720001079@std.bau.edu.jo>
2024-02-23 21:38:41 +0300

    refactor: Button and Card components

--- src/ui/Card.tsx
+++ src/ui/Card.tsx
@@ -7,9 +7,22 @@ interface CardProps extends HTMLAttributes<HTMLDivElement> {
 const Card: React.FC<CardProps> = ({ children, ...rest }) => (
   <div
     {...rest}
-    className="bg-white rounded-lg p-4 border-4 sm:w-64 md:w-56 lg:w-72 xl:w-80 2xl:w-80
+    className="bg-card rounded-3xl  p-4 border-4 sm:w-64 md:w-56 lg:w-72 xl:w-80 2xl:w-80
       h-auto w-full sm:h-64 md:h-72 lg:h-auto xl:h-auto 2xl:h-auto
-      cursor-pointer"
+      cursor-pointer
+      transition-all
+      hover:shadow-lg
+      hover:border-green-500
+      hover:scale-105
+      hover:duration-300
+      hover:ease-in-out
+      hover:transform
+      mb-4
+      sm:mb-0
+      md:mb-0
+      lg:mb-4
+      xl:mb-20
+      "
   >
     {children}
   </div>
