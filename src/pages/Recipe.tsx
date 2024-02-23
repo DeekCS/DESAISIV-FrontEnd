09f041e9ab1d05e757afae1da694ad6377f77b8f
deekCS <31720001079@std.bau.edu.jo>
2024-02-23 23:26:03 +0300

    feat: Add Recipe page

---
+++ src/pages/Recipe.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const Recipe: React.FC = () => {
+  const { id } = useParams()
+
+  return (
+    <div>
+      <h1>
+        Recipe
+        {id}
+      </h1>
+    </div>
+  )
+}
+
+export default Recipe
