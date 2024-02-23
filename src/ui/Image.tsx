ac4867868e45dfd6169afdfd8d5353a8c0f2ef42
deekCS <31720001079@std.bau.edu.jo>
2024-02-23 21:37:00 +0300

    feat: Update Image component dimensions

--- src/ui/Image.tsx
+++ src/ui/Image.tsx
@@ -11,7 +11,7 @@ const Image: React.FC<ImageProps> = ({ src }) => {
         className=" rounded-full overflow-hidden
         shadow-lg border-4 border-primary
         h-44
-         w-44 sm:w-44 sm:h-44 md:w-48 md:h-48 lg:w-52 lg:h-52 xl:w-60 xl:h-60 2xl:w-80 2xl:h-80
+         w-44 sm:w-44 sm:h-44 md:w-48 md:h-48 lg:w-52 lg:h-52 xl:w-60 xl:h-60 2xl:w-52 2xl:h-52
       "
       >
         <img className="object-cover w-full h-full" src={src} alt="Image" />
