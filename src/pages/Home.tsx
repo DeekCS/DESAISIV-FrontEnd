92b3693f9e516ce048e1d8f1a135ac875a47b2cf
deekCS <31720001079@std.bau.edu.jo>
2024-02-23 21:44:16 +0300

    feat: Add CategoryButtons, RecipeCard, RecipeCards, and Home components

---
+++ src/pages/Home.tsx
@@ -0,0 +1,34 @@
+import { useSearch } from '../context/SearchContext'
+import useCategory from '../hooks/useCategory'
+import useRecipes from '../hooks/useRecipes'
+import CategoryButtons from '../components/CategoryButtons'
+import RecipeCards from '../components/RecipeCards'
+import { categories } from '../utils/types'
+
+const Home = () => {
+  const { searchQuery } = useSearch()
+  const { selectedCategory, handleCategoryClick } = useCategory()
+  const { recipes, isLoading } = useRecipes(searchQuery, selectedCategory.name)
+
+  return (
+    <>
+      <div className="flex justify-center flex-row">
+        {categories.map((category) => (
+          <CategoryButtons
+            category={category}
+            onClick={handleCategoryClick}
+            isSelected={selectedCategory.name === category.name}
+          />
+        ))}
+      </div>
+
+      <RecipeCards
+        recipes={recipes!}
+        selectedCategory={selectedCategory}
+        isLoading={isLoading}
+      />
+    </>
+  )
+}
+
+export default Home
