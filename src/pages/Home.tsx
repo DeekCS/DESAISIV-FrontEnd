59e039fbee8d9101db5cc10c3c40bac94bc408b8
deekCS <31720001079@std.bau.edu.jo>
2024-02-24 03:27:38 +0300

    remove: useCategory hook and update import in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,15 @@
 import { useSearch } from '../context/SearchContext'
-import useCategory from '../hooks/useCategory'
 import useRecipes from '../hooks/useRecipes'
-import CategoryButtons from '../components/CategoryButtons'
 import RecipeCards from '../components/RecipeCards'
-import { categories } from '../utils/types'
+import useCategory from '../context/CategoryContext'
 
 const Home = () => {
   const { searchQuery } = useSearch()
-  const { selectedCategory, handleCategoryClick } = useCategory()
+  const { selectedCategory } = useCategory()
   const { recipes, isLoading } = useRecipes(searchQuery, selectedCategory.name)
 
   return (
     <>
-      <div className="flex justify-center flex-row">
-        {categories.map((category) => (
-          <CategoryButtons
-            key={category.id}
-            category={category}
-            onClick={handleCategoryClick}
-            isSelected={selectedCategory.name === category.name}
-          />
-        ))}
-      </div>
-
       <RecipeCards
         recipes={recipes!}
         selectedCategory={selectedCategory}
