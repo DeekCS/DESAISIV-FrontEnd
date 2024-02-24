c690cb4b93d302f069664366a5a05d6c40200367
deekCS <31720001079@std.bau.edu.jo>
2024-02-24 03:24:47 +0300

    feat: Add totalResults, instructions, and extendedIngredients to Recipe interface

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -4,12 +4,35 @@ export interface Category {
 }
 
 export interface Recipe {
+  id: number
   title: string
   image: string
   imageType: string
-  id: number
+  totalResults: number
+  instructions: string
+  extendedIngredients: Ingredient[]
 }
 
+export interface Ingredient {
+  id: number
+  original: string
+  image: string
+  name: string
+  amount: number
+  unit: string
+  measures: {
+    us: {
+      amount: number
+      unitShort: string
+      unitLong: string
+    }
+    metric: {
+      amount: number
+      unitShort: string
+      unitLong: string
+    }
+  }
+}
 export const categories: Category[] = [
   { id: '1', name: 'Indian' },
   { id: '2', name: 'Mexican' },
