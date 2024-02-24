111c4501ac0a09326a42ca5ecb7f7a97a08d6460
deekCS <31720001079@std.bau.edu.jo>
2024-02-24 03:26:06 +0300

    style: Update Button styles

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -13,13 +13,14 @@ const Button: FC<ButtonProps> = ({
 }) => {
   const buttonClasses = `${
     recipesStyle
-      ? `bg-tertiary text-secondary hover:bg-secondary hover:text-tertiary hover:scale-105 mt-4 sm:mt-0 md:mt-0 lg:mt-0 xl:mt-4 2xl:mt-4 rounded-md px-6 py-2 font-bold tracking-wider transition duration-300 ease-in-out transform`
+      ? `bg-tertiary text-secondary hover:bg-secondary hover:text-tertiary hover:scale-105 mt-4 sm:mt-0 md:mt-0 lg:mt-0 xl:mt-4 2xl:mt-4 rounded-md px-6 py-2 font-bold tracking-wider transition duration-300 ease-in-out transform cursor-pointer
+      `
       : isSelected
         ? `bg-black border-2 text-white font-bold py-2 px-6 rounded-full transition-all duration-300 ease-in-out transform hover:bg-black hover:text-white hover:scale-105
-        ml-2 sm:ml-0 md:ml-0 lg:ml-0 xl:ml-2 2xl:ml-2
+        ml-2 sm:ml-0 md:ml-0 lg:ml-0 xl:ml-2 2xl:ml-2 cursor-pointer
         `
         : `border-2 text-gray-700 font-bold py-2 px-6 rounded-full transition-all duration-300 ease-in-out transform hover:bg-black hover:text-white hover:scale-105
-        ml-2 sm:ml-0 md:ml-0 lg:ml-0 xl:ml-2 2xl:ml-2
+        ml-2 sm:ml-0 md:ml-0 lg:ml-0 xl:ml-2 2xl:ml-2 cursor-pointer
         `
   }`
 
