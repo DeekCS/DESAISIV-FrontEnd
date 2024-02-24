e83e0a32aad4e939d1fc6550bfd0b328ac00a242
deekCS <31720001079@std.bau.edu.jo>
2024-02-24 03:27:16 +0300

    feat: Add CategoryList component to AppLayout

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Header from '../ui/Header'
 import { Outlet } from 'react-router-dom'
 import SearchBar from '../ui/SearchBar'
+import CategoryList from '../components/CategoryList'
 
 const AppLayout: React.FC = () => {
   return (
     <div className="min-h-screen bg-primary p-8" data-testid="app-layout">
       <SearchBar placeholder="Search for recipes" />
       <Header />
-
+      <CategoryList />
       <main className="flex-grow">
         <Outlet />
       </main>
